24c0d93cb688cd4a05b29b138029143ccdf84403
Deyan Tsvetanov <tsvetd1>
2024-01-07 16:58:05 +0200

    Remove unused imports.

--- src/main/java/com/example/empay/config/JpaConfig.java
+++ src/main/java/com/example/empay/config/JpaConfig.java
@@ -1,16 +1,10 @@
 package com.example.empay.config;
 
-import com.example.empay.entity.security.UserLogin;
-import com.example.empay.security.EmpayUserDetails;
 import com.example.empay.util.Constants;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.auditing.DateTimeProvider;
-import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.time.ZonedDateTime;
 import java.util.Optional;
@@ -22,12 +16,6 @@ import java.util.Optional;
 @EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider",
         modifyOnCreate = false)
 public class JpaConfig {
-
-    /**
-     * Persistence context used to load the user entity.
-     */
-    @PersistenceContext
-    private EntityManager entityManager;
 
     /**
      * Provides <code>ZonedDateTime</code> in UTC timezone for JPA entity
